106a3cc936c52495e8603a5b267fcd97b0cbb27a
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-05 18:46:59 +0530

    XAMARIN-34871 - Sample and screenshot added.

---
+++ CalendarXamarin/CalendarXamarin/CalendarXamarin/Behavior/CalendarPageBehavior.cs
@@ -0,0 +1,20 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CalendarXamarin
+{
+    public class CalendarPageBehavior : Behavior<ContentPage>
+    {
+        SfCalendar calendar;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            calendar = bindable.FindByName<SfCalendar>("calendar");
+            calendar.Locale = new System.Globalization.CultureInfo("AR");
+        }
+    }
+}
+    
